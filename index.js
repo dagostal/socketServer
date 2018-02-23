9d3d67736cf4337b4e94837e4d5cb00172431f29
alex <dagostal@bc.edu>
2018-02-23 14:04:40 -0500

    g

--- index.js
+++ index.js
@@ -19,18 +19,19 @@ wsServer.on('request', function(request) {
 
   var connection = request.accept(null, request.origin);
 
-    var obj = {
-             text: "message from server",
-           };
-    var json = JSON.stringify({ type:'message', data: obj });
-
-    connection.sendUTF(json)
 
   // This is the most important callback for us, we'll handle
   // all messages from users here.
   connection.on('message', function(message) {
     console.log('message received!')
     console.log(message)
+
+        var obj = {
+                 text: "nice to meet u, this is the server",
+               };
+        var json = JSON.stringify({ type:'message', data: obj });
+
+        connection.sendUTF(json)
   });
 
   connection.on('close', function(connection) {
