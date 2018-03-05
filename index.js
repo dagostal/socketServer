e6763510c598a4839ffb7a6ae7875872101151c0
alex <dagostal@bc.edu>
2018-03-05 16:56:03 -0500

    g

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ wsServer.on('request', function(request) {
     console.log(message.utf8Data)
     if(message.utf8Data.latitude!==location.latitude||message.utf8Data.longitude!==location.longitude) {
       console.log('location change')
-      console.log(message.utf8Data.latitude,message.utf8Data.longitude)
+      JSON.parse(message.utf8Data)
+      console.log(message.utf8Data)
       location={
         latitude:message.utf8Data.latitude,
         longitude:message.utf8Data.longitude
