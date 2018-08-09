9c90f84b0924bc76d80426a829ff06c37b67102f
alex <dagostal@bc.edu>
2018-08-09 14:38:24 -0400

    g

--- index.js
+++ index.js
@@ -20,33 +20,33 @@ wsServer.on('request', function(request) {
   console.log('connection requested')
   var connection = request.accept(null, request.origin);
 
-  connections.push(connection)
+  // connections.push(connection)
 
   // This is the most important callback for us, we'll handle
   // all messages from users here.
   connection.on('message', function(message) {
     console.log('message received!')
-
-    var messageData=JSON.parse(message.utf8Data)
-    if(messageData.type==="location"){
-    console.log(messageData)
-    var latitude=messageData.latitude
-    var longitude=messageData.longitude
-    var busId=messageData.busId
-      locationDataToParent={
-        latitude:latitude,
-        longitude:longitude,
-        busId:busId
-      }
-
-
-        var json = JSON.stringify({ type:'message', data: locationDataToParent });
-
-        connections.forEach((connect)=>{
-            connect.sendUTF(json)
-        })
-        console.log("connections:",connections.length)
-      }
+    console.log(JSON.parse(message.utf8Data))
+    // var messageData=JSON.parse(message.utf8Data)
+    // if(messageData.type==="location"){
+    // console.log(messageData)
+    // var latitude=messageData.latitude
+    // var longitude=messageData.longitude
+    // var busId=messageData.busId
+    //   locationDataToParent={
+    //     latitude:latitude,
+    //     longitude:longitude,
+    //     busId:busId
+    //   }
+    //
+    //
+    //     var json = JSON.stringify({ type:'message', data: locationDataToParent });
+    //
+    //     connections.forEach((connect)=>{
+    //         connect.sendUTF(json)
+    //     })
+    //     console.log("connections:",connections.length)
+    //   }
   });
 
   connection.on('close', function(connection) {
