25ebd56070a5f734238436a6dd49dd89f06b459a
alex <dagostal@bc.edu>
2018-08-26 13:25:32 -0400

    socket

--- bluetooth.js
+++ bluetooth.js
@@ -9,7 +9,7 @@ var server = http.createServer(function(request, response) {
   // process HTTP request. Since we're writing just WebSockets
   // server we don't have to implement anything.
 });
-server.listen(port, function() { });
+server.listen(port, function() { console.log("socket server is on listneing on port..",port)});
 
 // create the server
 wsServer = new WebSocketServer({
@@ -40,16 +40,36 @@ wsServer.on('request', function(request) {
 
     if(existingConnection){ //if this connection already exists...
       console.log("connection exists!")
+      connection.count=0;
+      return;
     } else {   //if there is no exisitng connection...
       console.log("connection does not exist!")
       bluetoothConnections.push(obj)
+      //send alert to child-on-bus route
     }
-
-
   })
 });
 
-//   connection.on('close', function(connection) {
-//     // close user connection
-//   });
-// // });
+  connection.on('close', function(connection) {
+    // close user connection
+    console.log("socket server was closed...")
+  });
+
+var incrementConnections=function(){
+  bluetoothConnections.forEach((connection,i)=>{
+    connection.count++
+    if(connection.count>21){
+      bluetoothConnections.splice(i,1)
+      //we have not scaned the child in 40 seconds...he must be off the bus...
+      //remove from connectionList and alert off-bus route
+    }
+  })
+}
+
+
+
+
+
+
+
+//
